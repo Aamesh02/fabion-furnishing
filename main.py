4474c7eaf91f27b7520ed31e878a13cd2c51371b
Aamesh02 <aameshgori2002@gmail.com>
2023-11-16 14:05:09 +0530

    image shuffling and making responsive

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import os
 MAIL_ADDRESS = os.environ.get('MAIL_ID')
 MAIL_APP_PW = os.environ.get('MAIL_PW')
 COMPANY_ADDRESS = os.environ.get('COMPANY_ID')
+PHONE = os.environ.get('PHONE')
+
+MAIL_ADDRESS = 'aameshth2@gmail.com'
+MAIL_APP_PW = 'rwik xdbm hdds ezcw'
+COMPANY_ADDRESS = 'fabionfurnishing@gmail.com'
+PHONE = '+919930456635'
 
 app = Flask(__name__)
 
@@ -14,14 +20,17 @@ def home():
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('message')
-        with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
-            connection.starttls()
-            connection.login(user=MAIL_ADDRESS, password=MAIL_APP_PW)
-            connection.sendmail(from_addr=MAIL_ADDRESS,
-                                to_addrs=COMPANY_ADDRESS,
-                                msg=f'Subject:Website Contact\n\nName: {name}\nEmail: {email}\nMessage: {message}')
+        try:
+            with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+                connection.starttls()
+                connection.login(user=MAIL_ADDRESS, password=MAIL_APP_PW)
+                connection.sendmail(from_addr=MAIL_ADDRESS,
+                                    to_addrs=COMPANY_ADDRESS,
+                                    msg=f'Subject:Website Contact\n\nName: {name}\nEmail: {email}\nMessage: {message}')
+        except Exception as e:
+            print(f"An error occurred: {e}")
         
-    return render_template('index.html')
+    return render_template('index.html', phone=PHONE, mail=COMPANY_ADDRESS)
 
 if __name__ == '__main__':
     app.run(debug=False, port=5001)
